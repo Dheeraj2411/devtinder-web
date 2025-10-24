68fc50793824cc1c94ff4cbbd95e9d6548284ec8
Dheeraj2411 <dheerajjha834@gmail.com>
2025-10-24 19:00:58 +0530

    API credentials update and cookies store

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
 # DevTinder
 
- - create devtinder web vite react project
- - install tailwind
- - git init
- - install daisy UI
- - add navbar in App.js from daisyUI
- - Navbar separate
- - npm i react-router-dom (for frontend routing)
+- create devtinder web vite react project
+- install tailwind
+- git init
+- install daisy UI
+- add navbar in App.js from daisyUI
+- Navbar separate
+- npm i react-router-dom (for frontend routing)
+- create BrowserRouter > Routes >Route=/Body > RouteChildren
+- Create an Outlet in Body component
+- create Footer
+- create login Page
+- npm i axios
+- CORS - npm i cors in backend => add middleware to configurations:origin,credentials
+- Making API call Axios => withcredentials:true
 
+Bdoy
+Navbar
+Route=/ =>Feed
+Route=/login=>Login
+Route=/connections=>connections
+Router=/profile =>Profile
 
- # Routes
-  - create body component
-  - three component in body navbar,<Outlet>(routing the children),Footer
-  - setup routing in App.js component <BrowserRoute> </BrowserRoute> 
-    - in browserroute call <rotutes/><rotutes/>
-       - in routes call route for giving path and element <Route></Route>(add many parent route)
-            - Add chlid router (add one or more children)
+# Routes
+
+- create body component
+- three component in body navbar,<Outlet>(routing the children),Footer
+- setup routing in App.js component <BrowserRoute> </BrowserRoute>
+  - in browserroute call <rotutes/><rotutes/>
+    - in routes call route for giving path and element <Route></Route>(add many parent route)
+      - Add chlid router (add one or more children)
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("mark123@gmail.com");
+  const [password, setPassword] = useState("Mark@123");
+  const handleLogin = async () => {
+   try {
+     const res = await axios.post("http://localhost:7777/login", {
+       email,
+       password,
+     },{withCredentials:true});
+    
+   } catch (error) {
+    console.error(error)
+   }
+  };
 
-export default Login
+  return (
+    <div className="flex justify-center my-4">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email ID</legend>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input"
+              placeholder="@ mail"
+            />
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">password</legend>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input"
+              placeholder="password"
+            />
+          </fieldset>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
